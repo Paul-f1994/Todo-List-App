5fd99921b0b206504d710a1cfc64fc30f8db8cdd
Paul Feng <paulfeng@google.com>
2022-07-20 16:54:49 -0700

    Add due for each task and update README

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 #### Setup
 <hr>
 
-- Create python3 virtual environment and activate it (python3 -m venv venv)
-- Run 'pip install -r requirements.txt' on terminal
-- Run main.py on terminal for start local server
+- Create python3 virtual environment ```python3 -m venv venv```
+- Activate virtual environment with ```source venv/bin/activate```
+- Run ```pip install -r requirements.txt`` on terminal
+- Export GOOGLE_APPLICATION_CREDENTIALS to grant access to the ndb. ```export GOOGLE_APPLICATION_CREDENTIALS="/Users/paulfeng/Downloads/utopian-river-195906-097620e8d4b5.json"```
+- Run ```python main.py``` on terminal for start local server
 
 <hr>
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class TodoList(ndb.Model):
     id = ndb.IntegerProperty()
     title = ndb.StringProperty()
     complete = ndb.BooleanProperty()
+    due = ndb.StringProperty()
 
 @app.route("/")
 def index():
@@ -38,12 +39,13 @@ def init():
 @app.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
+    due = request.form.get("due")
     with client.context():
         todo = TodoList.query().order(-TodoList.id).get()
         if todo != None:
-            new_todo = TodoList(id = todo.id + 1, title = title, complete = False)
+            new_todo = TodoList(id = todo.id + 1, title = title, complete = False, due = due)
         else:
-            new_todo = TodoList(id = 0, title = title, complete = False)
+            new_todo = TodoList(id = 0, title = title, complete = False, due = due)
         new_todo.put()
         return redirect(url_for("index"))
 
@@ -67,8 +69,5 @@ def delete(todo_id):
         return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    # db.create_all()
     client
     app.run(debug=True)
-
-# export GOOGLE_APPLICATION_CREDENTIALS="/Users/paulfeng/Downloads/utopian-river-195906-097620e8d4b5.json"
